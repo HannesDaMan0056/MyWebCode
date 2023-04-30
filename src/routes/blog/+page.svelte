2607dc6dabf947c860052ddf05fe19e9e1ebb41c
walczykm <walczyk.marcin@gmail.com>
2023-04-30 09:18:08 +0200

    BS-1326 Reformatted in prettier and added blog page.

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,132 @@
+<section class="bg-white dark:bg-gray-900">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+      <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Skillbased Blog</h2>
+      <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We strive to connect with the needs of your audience early and often.</p>
+    </div>
+    <div class="grid gap-8 lg:grid-cols-2">
+      <article class="p-6 bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex justify-between items-center mb-5 text-gray-500">
+                  <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                      <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd"
+                                                                                                                                 d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                                                                                                                                 clip-rule="evenodd"></path><path
+                        d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
+                      Article
+                  </span>
+          <span class="text-sm">May 2, 2023</span>
+        </div>
+        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="/blog/why-skillbased.html">Why Skillbased</a></h2>
+        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+          When working with Skillbased Consulting, you will be assigned a senior consultant with expert industry experience to advise and guide you throughout the process and service
+          delivery. We work closely with you to get a thorough understanding of your vision, goals, prerequisites, and requirements. Our goal is to deliver the most relevant skills based on
+          your needs and requirements.
+        </p>
+        <div class="flex justify-between items-center">
+          <div class="flex items-center space-x-4">
+            <img class="w-7 h-7 rounded-full" src="/assets/images/ProfilePictures/kristian.jpg" alt="Kristian Veem">
+            <span class="font-medium dark:text-white">
+                          Kristian Veem
+                      </span>
+          </div>
+          <a href="/blog/why-skillbased.html" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+            Read more
+            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+            </svg>
+          </a>
+        </div>
+      </article>
+      <article class="p-6 bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex justify-between items-center mb-5 text-gray-500">
+                  <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                      <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path
+                        d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                      Tutorial
+                  </span>
+          <span class="text-sm">May 3, 2023</span>
+        </div>
+        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="/blog/registration.html">Registration</a></h2>
+        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+          If you're interested in creating a free Skillbased consultant account, registration is the first step. Here's a simple guide on how to create an account through registration: <br>
+          Go to the Skillbased website and hover over "Register" or "Sign up" in the top navigation menu.
+        </p>
+        <div class="flex justify-between items-center">
+          <div class="flex items-center space-x-4">
+            <img class="w-7 h-7 rounded-full" src="/assets/images/ProfilePictures/samuel.jpg" alt="Samuel Veem">
+            <span class="font-medium dark:text-white">
+                          Samuel Veem
+                      </span>
+          </div>
+          <a href="/blog/registration.html" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+            Read more
+            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+            </svg>
+          </a>
+        </div>
+      </article>
+      <article class="p-6 bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex justify-between items-center mb-5 text-gray-500">
+                  <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                      <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path
+                        d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                      Tutorial
+                  </span>
+          <span class="text-sm">May 3, 2023</span>
+        </div>
+        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="/blog/importing-profile.html">Importing profile</a></h2>
+        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+          Are you looking to quickly populate your Skillbased profile with your LinkedIn information? Here's a step-by-step guide on how to do it: <br>
+          Log into LinkedIn and view your profile.
+        </p>
+        <div class="flex justify-between items-center">
+          <div class="flex items-center space-x-4">
+            <img class="w-7 h-7 rounded-full" src="/assets/images/ProfilePictures/hannes.png" alt="Johannes Veem">
+            <span class="font-medium dark:text-white">
+                          Johannes Veem
+                      </span>
+          </div>
+          <a href="/blog/importing-profile.html" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+            Read more
+            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+            </svg>
+          </a>
+        </div>
+      </article>
+      <article class="p-6 bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex justify-between items-center mb-5 text-gray-500">
+                  <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                      <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path
+                        d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                      Tutorial
+                  </span>
+          <span class="text-sm">May 3, 2023</span>
+        </div>
+        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="/blog/work-experience.html">Work Experience</a></h2>
+        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+          When registering your work experiences and projects on Skillbased, it's important to be as detailed as possible and keep them updated. Here's what you need to know:
+        </p>
+        <div class="flex justify-between items-center">
+          <div class="flex items-center space-x-4">
+            <img class="w-7 h-7 rounded-full" src="/assets/images/ProfilePictures/kristian.jpg" alt="Kristian Veem">
+            <span class="font-medium dark:text-white">
+                          Kristian Veem
+                      </span>
+          </div>
+          <a href="/blog/work-experience.html" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+            Read more
+            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+            </svg>
+          </a>
+        </div>
+      </article>
+    </div>
+  </div>
+</section>
