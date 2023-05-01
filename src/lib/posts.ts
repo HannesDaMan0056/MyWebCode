4895626583a7f1788c33107606e787d91b2d5f80
walczykm <walczyk.marcin@gmail.com>
2023-05-01 21:25:16 +0200

    BS-1326 Post Cards to show summaries of all blog posts.

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -11,28 +11,33 @@ export interface Author {
   image: string;
 }
 
+export type PostType = "article" | "tutorial"
+
 export interface Post {
   title: string;
   pubdate: string;
   author: Author;
+  slug: string;
+  summary: string;
+  type: PostType;
 }
 
 export const kristian: Author = {
   name: "Kristian Veem",
   title: "Managing Director / CEO Skillbased",
-  image: kristianImage
+  image: kristianImage,
 };
 
 export const samuel: Author = {
   name: "Samuel Veem",
   title: "Technical Writer / Multimedia Developer",
-  image: samuelImage
+  image: samuelImage,
 };
 
 export const johannes: Author = {
   name: "Johannes Veem",
   title: "Technical Writer / Tester",
-  image: johannesImage
+  image: johannesImage,
 };
 
 export type PostSlug = "why-skillbased" | "registration" | "importing-profile" | "work-experience";
@@ -41,20 +46,37 @@ export const posts: Record<PostSlug, Post> = {
     title: "Why Skillbased?",
     author: kristian,
     pubdate: "2023-05-01",
+    type: "article",
+    slug: "/blog/why-skillbased",
+    summary: `When working with Skillbased Consulting, you will be assigned a senior consultant with expert industry experience to advise and guide you throughout the process and service delivery. 
+    We work closely with you to get a thorough understanding of your vision, goals, prerequisites, and requirements. Our goal is to deliver the most relevant skills based on your needs and requirements.`,
   },
   "registration": {
     title: "Registration",
     author: samuel,
     pubdate: "2023-05-02",
+    type: "tutorial",
+    slug: "/blog/registration",
+    summary: `If you're interested in creating a free Skillbased consultant account, registration is the first step. 
+Here's a simple guide on how to create an account through registration: <br> 
+Go to the Skillbased website and hover over "Register" or "Sign up" in the top navigation menu."`,
   },
   "importing-profile": {
     title: "Importing Profile",
     author: johannes,
     pubdate: "2023-05-02",
+    type: "tutorial",
+    slug: "/blog/importing-profile",
+    summary: `Are you looking to quickly populate your Skillbased profile with your LinkedIn information? 
+Here's a step-by-step guide on how to do it: <br>
+          Log into LinkedIn and view your profile.`,
   },
   "work-experience": {
     title: "Work Experience",
     author: kristian,
     pubdate: "2023-05-02",
+    type: "tutorial",
+    slug: "/blog/work-experience",
+    summary: `When registering your work experiences and projects on Skillbased, it's important to be as detailed as possible and keep them updated. Here's what you need to know:`,
   },
 };
