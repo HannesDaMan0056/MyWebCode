a8fbde195432a9105841550f19e4a76fe3cda369
walczykm <walczyk.marcin@gmail.com>
2023-06-27 15:14:54 +0200

    BS-1326 Black navigation in blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, Button } from 'flowbite-svelte';
-  let navClass = 'bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800';
+  let navClass = 'dark bg-black border-gray-200 px-4 lg:px-6 dark:bg-gray-800';
   let navDivClass = 'flex flex-wrap justify-between items-center mx-auto max-w-screen-xl';
   const breadcrumb_title = 'Headers';
   const title = 'Header';
@@ -10,13 +10,13 @@
   import "../app.postcss";
   import "../app.postcss";
 
-  import logo from "$lib/images/white-logo.png?webp&h-9";
+  import logo from "$lib/images/black-logo.png";
 </script>
 
 <header>
   <Navbar let:hidden let:toggle fluid={false} {navClass} {navDivClass}>
     <NavBrand href="/" data-sveltekit-preload-data="off" >
-        <img src={logo} class="mr-3 h-9" alt="Skillbased Logo" />
+        <img src={logo} class="mr-3 h-14" alt="Skillbased Logo" />
     </NavBrand>
     <div class="flex items-center lg:order-2">
       <NavHamburger
@@ -27,7 +27,7 @@
     <NavUl
       {hidden}
       divClass="justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-      ulClass="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"
+      ulClass="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 uppercase"
     >
       <NavLi href="/">Home</NavLi>
       <NavLi href="/client">Client</NavLi>
