d375425f1d7c6698ff74a58f4fce1162f14bd18d
walczykm <walczyk.marcin@gmail.com>
2023-10-10 20:27:58 +0200

    BS-1376 Hidden not working newsletter signup.

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,6 +1,6 @@
 <slot></slot>
 
-<section class="bg-white dark:bg-gray-900">
+<section class="hidden">
   <div class="pb-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div class="mx-auto max-w-screen-md sm:text-center">
       <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Sign up for our newsletter</h2>
