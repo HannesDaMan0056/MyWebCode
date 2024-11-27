ae6d873f9cbc1ff594891a549da81b3f932f4a8f
walczykm <walczyk.marcin@gmail.com>
2024-11-27 22:54:21 +0100

    BS-1491 Fixed links, changed blog to guide

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -5,7 +5,7 @@
 <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-      <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Skillbased Blog</h2>
+      <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Skillbased Guide</h2>
       <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We strive to connect with the needs of your audience early and often.</p>
     </div>
     <div class="grid gap-8 lg:grid-cols-2">
